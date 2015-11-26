6bd0c485bbed52d19d9c1d5745b3e4d996ed3b03
Lawrence Park <parklawr@ug132.eecg>
2015-11-25 20:57:25 -0500

    hey

--- simplesim-3.0d-assig4/mbq1.c
+++ simplesim-3.0d-assig4/mbq1.c
@@ -10,16 +10,13 @@ int main(int argc, char *argv[])
 {
  int i,j;
  int size = atoi(argv[1]);
- printf("Size of struct : %d", (int)sizeof(struct blk_data));
 
- struct blk_data b_array[8];
+ struct blk_data b_array[size];
  for(i=0; i < size; i=i+1)
  {
-   i = i & 7;
    for(j=0; j < 16; j=j+1)
    {
      b_array[i].data[j] = i & j;
-     b_array[i].data[j]++;
    }
  }
 
